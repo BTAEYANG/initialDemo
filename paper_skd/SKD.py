42c22403399b640f3b9070ddc73f5b90f94a1a4b
BTAEYANG <619217271@qq.com>
2022-09-15 22:28:08 +0800

    SKD

--- paper_skd/SKD.py
+++ paper_skd/SKD.py
@@ -44,9 +44,23 @@ class SKD(nn.Module):
         relation_matrix[range(len(f)), range((len(f)))] = 0
         return relation_matrix
 
+    @staticmethod
+    def cov_ma(f):
+        _, c, h, w = f.shape
+        f = torch.stack(f.view(f.shape[0], -1).split(1, dim=0))
+        t_matrix_tuple = torch.bmm(f.transpose(1, 2), f).split(1, dim=0)
+        f_cov = torch.stack([torch.cov(i.view(c * h * w, c * h * w)) for i in t_matrix_tuple])
+        return f_cov
+
     def forward(self, f_t, f_s):
         # initial feature channel
         b, _, _, _ = f_t[0].shape
+
+        # cov relation
+        with torch.no_grad():
+            cov_m_t = torch.stack([self.cov_ma(i) for i in f_t])
+
+        cov_m_s = torch.stack([self.cov_ma(i) for i in f_s])
 
         f_t = [i.view(i.shape[0], -1) for i in f_t]
         f_s = [i.view(i.shape[0], -1) for i in f_s]
@@ -65,15 +79,19 @@ class SKD(nn.Module):
         # sample angle relation
         with torch.no_grad():
             td = [i.unsqueeze(0) - i.unsqueeze(1) for i in f_t]
-            t_angle = torch.stack([torch.bmm(F.normalize(i, p=2, dim=2), F.normalize(i, p=2, dim=2).transpose(1, 2)) for i in td]).transpose(0, 1)
+            t_angle = torch.stack(
+                [torch.bmm(F.normalize(i, p=2, dim=2), F.normalize(i, p=2, dim=2).transpose(1, 2)) for i in
+                 td]).transpose(0, 1)
 
         sd = [i.unsqueeze(0) - i.unsqueeze(1) for i in f_s]
-        s_angle = torch.stack([torch.bmm(F.normalize(i, p=2, dim=2), F.normalize(i, p=2, dim=2).transpose(1, 2)) for i in sd]).transpose(0, 1)
+        s_angle = torch.stack(
+            [torch.bmm(F.normalize(i, p=2, dim=2), F.normalize(i, p=2, dim=2).transpose(1, 2)) for i in sd]).transpose(
+            0, 1)
 
         # structure stage distance relation
         # with torch.no_grad():
 
-        return relation_t_d, relation_s_d, t_angle, s_angle
+        return relation_t_d, relation_s_d, t_angle, s_angle, cov_m_t, cov_m_s
 
 
 class SKD_Loss(nn.Module):
@@ -100,7 +118,7 @@ class SKD_Loss(nn.Module):
             loss += self.mse(s, t)
         return loss
 
-    def forward(self, r_t_d, r_s_d, r_t_a, r_s_a):
+    def forward(self, r_t_d, r_s_d, r_t_a, r_s_a, cov_m_t, cov_m_s):
 
         loss_d_s = self._spatial_mean_loss(r_t_d, r_s_d)
         loss_d_c = self._channel_mean_loss(r_t_d, r_s_d)
@@ -108,30 +126,39 @@ class SKD_Loss(nn.Module):
         loss_a_s = self._spatial_mean_loss(r_t_a, r_s_a)
         loss_a_c = self._channel_mean_loss(r_t_a, r_s_a)
 
-        loss = loss_d_s + loss_d_c + loss_a_s + loss_a_c
+        loss_cov_s = self._spatial_mean_loss(cov_m_t, cov_m_s)
+        loss_cov_c = self._channel_mean_loss(cov_m_t, cov_m_s)
+
+        loss = loss_d_s + loss_d_c + loss_a_s + loss_a_c + loss_cov_s + loss_cov_c
 
         return loss
 
 
 if __name__ == '__main__':
-    x = torch.randn(10, 3, 32, 32)
+    pass
+    # x = torch.randn(10, 3, 32, 32)
+    #
+    # b, _, _, _ = x.shape
+    #
+    # net = resnet32x4(num_classes=100)
+    #
+    # feats, logit = net(x, is_feat=True, preact=True)
+    #
+    # feats.append(logit)
+    #
+    # stage = len(feats)
+    #
+    # with torch.no_grad():
+    #     f_t = [i.view(i.shape[0], -1) for i in feats]
+    #     f_t = [SKD._relation_dist(i) for i in f_t]
+    #     relation_t_d = torch.stack(f_t).view(1, stage, b, b)
+    #     print(relation_t_d.shape)
+    #
+    #     cov_m = [cov_ma(i) for i in feats]
+    #     cov_m_t = torch.stack(cov_m)
+    #     print(cov_m_t.shape)
 
-    net = resnet32x4(num_classes=100)
-
-    feats, logit = net(x, is_feat=True, preact=True)
-
-    feats.append(logit)
-
-    b, _, _, _ = feats[0].shape
-    stage = len(feats)
-
-    with torch.no_grad():
-        f_t = [i.view(i.shape[0], -1) for i in feats]
-        f_t = [SKD._relation_dist(i) for i in f_t]
-        relation_t_d = torch.stack(f_t).view(1, stage, b, b)
-        print(relation_t_d.shape)
-
-        # f_t = [i.view(i.shape[0], -1) for i in feats]
-        td = [i.unsqueeze(0) - i.unsqueeze(1) for i in f_t]
-        t_angle = torch.stack([torch.bmm(F.normalize(i, p=2, dim=2), F.normalize(i, p=2, dim=2).transpose(1, 2)) for i in td])
-        print(t_angle.shape)
+    # f_t = [i.view(i.shape[0], -1) for i in feats]
+    # td = [i.unsqueeze(0) - i.unsqueeze(1) for i in f_t]
+    # t_angle = torch.stack([torch.bmm(F.normalize(i, p=2, dim=2), F.normalize(i, p=2, dim=2).transpose(1, 2)) for i in td])
+    # print(t_angle.shape)
