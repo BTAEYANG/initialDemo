7ccc01eeef8537eb0d03018be192f816e97266ee
BTAEYANG <619217271@qq.com>
2022-09-24 21:59:37 +0800

    SKD SKDLoss

--- paper_skd/SKD.py
+++ paper_skd/SKD.py
@@ -21,7 +21,8 @@ class SKD(nn.Module):
             t_channel_relation, t_channel_pearson = self.stage_channel_pearson(f_t)
             t_sample_relation, t_sample_pearson = self.stage_sample_pearson(f_t)
 
-        return t_spatial_pearson, s_spatial_pearson, t_channel_pearson, s_channel_pearson, t_sample_pearson, s_sample_pearson
+        # return t_spatial_pearson, s_spatial_pearson, t_channel_pearson, s_channel_pearson, t_sample_pearson, s_sample_pearson
+        return t_spatial_pearson, s_spatial_pearson
 
     @staticmethod
     def stage_spatial_pearson(f):
@@ -100,20 +101,20 @@ class SKD_Loss(nn.Module):
         elif loss_type == 'L1':
             self.loss = nn.L1Loss()
 
-    def forward(self, t_spatial_pearson, s_spatial_pearson, t_channel_pearson, s_channel_pearson, t_sample_pearson, s_sample_pearson):
+    def forward(self, t_spatial_pearson, s_spatial_pearson):
 
         spatial_pearson_loss = sum(self.loss(i, j) for i, j in zip(t_spatial_pearson, s_spatial_pearson))
+        #
+        # channel_pearson_loss = sum(self.loss(i, j) for i, j in zip(t_channel_pearson, s_channel_pearson))
+        #
+        # sample_pearson_loss = sum(self.loss(i, j) for i, j in zip(t_sample_pearson, s_sample_pearson))
 
-        channel_pearson_loss = sum(self.loss(i, j) for i, j in zip(t_channel_pearson, s_channel_pearson))
+        # loss = [spatial_pearson_loss, channel_pearson_loss, sample_pearson_loss]
+        #
+        # factor = F.softmax(torch.Tensor(loss), dim=-1)
+        # loss_t = sum(factor[index] * loss[index] for index, value in enumerate(loss))
 
-        sample_pearson_loss = sum(self.loss(i, j) for i, j in zip(t_sample_pearson, s_sample_pearson))
-
-        loss = [spatial_pearson_loss, channel_pearson_loss, sample_pearson_loss]
-
-        factor = F.softmax(torch.Tensor(loss), dim=-1)
-        loss_t = sum(factor[index] * loss[index] for index, value in enumerate(loss))
-
-        return loss_t
+        return spatial_pearson_loss
 
 
 if __name__ == '__main__':
