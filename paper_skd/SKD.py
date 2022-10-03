6dfe8314a079bc3cf4206b20b7d1d6e2184818c9
BTAEYANG <619217271@qq.com>
2022-10-03 19:34:48 +0800

    SKD SKDLoss

--- paper_skd/SKD.py
+++ paper_skd/SKD.py
@@ -55,12 +55,12 @@ class SKD(nn.Module):
         stage_list_s = self.compute_stage(f_s)
 
 
-        with torch.no_grad():
-            for i in range(len(stage_list_t)):
-                stage_list_t[i] = self.embedding_l[i](stage_list_t[i])
-
-        for i in range(len(stage_list_t)):
-            stage_list_s[i] = self.embedding_l[i](stage_list_s[i])
+        # with torch.no_grad():
+        #     for i in range(len(stage_list_t)):
+        #         stage_list_t[i] = self.embedding_l[i](stage_list_t[i])
+        #
+        # for i in range(len(stage_list_t)):
+        #     stage_list_s[i] = self.embedding_l[i](stage_list_s[i])
 
         t_tensor = torch.stack(stage_list_t)
         s_tensor = torch.stack(stage_list_s)
