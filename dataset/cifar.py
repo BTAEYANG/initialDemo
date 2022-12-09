f4606579081529fd451b5fb69dbc6ad84679a840
BTAEYANG <619217271@qq.com>
2022-12-09 13:23:51 +0800

    GSKD

--- dataset/cifar.py
+++ dataset/cifar.py
@@ -8,8 +8,8 @@ def get_data_folder(opt):
     """
     return server-dependent path to store the data
     """
-    root_path = 'E:/home/lab265/lab265/datasets'  # windows
-    # root_path = '/home/lab265/lab265/datasets'  # v100 linux
+    # root_path = 'E:/home/lab265/lab265/datasets'  # windows
+    root_path = '/home/lab265/lab265/datasets'  # v100 linux
     # root_path = '/Disk1/datasets'  # 1080 linux
     data_folder = os.path.join(root_path, opt.dataset)
 
@@ -52,8 +52,8 @@ def getDataLoader(opt):
     else:
         assert False
     train_loader = torch.utils.data.DataLoader(dataset=train_dataset, batch_size=opt.batch_size,
-                                               shuffle=True, pin_memory=True, num_workers=8)
+                                               shuffle=True, pin_memory=True, num_workers=4)
     test_loader = torch.utils.data.DataLoader(dataset=test_dataset, batch_size=opt.batch_size,
-                                              shuffle=False, pin_memory=True, num_workers=8)
+                                              shuffle=False, pin_memory=True, num_workers=4)
 
     return train_loader, test_loader, num_classes
--- paper/GSKD.py
+++ paper/GSKD.py
@@ -4,50 +4,65 @@ import torch.nn.functional as F
 
 from dataset.cifar import getDataLoader
 import argparse
+
+from models import mobile_half, resnet32x4
 from paper.GKD import GKD
 from train_student import parse_option
+from util.embedding_util import MLPEmbed
 
 
 class GSKD(nn.Module):
     """Guided Similarity Knowledge Distillation"""
 
-    def __init__(self):
+    def __init__(self, y_s, y_t):
         super(GSKD, self).__init__()
+        self.embed_s = nn.ModuleList([])
+        for s in y_s:
+            h = s.shape[2]
+            self.embed_s.append(MLPEmbed(dim_in=h * h, dim_out=y_t.shape[1]))
 
     @staticmethod
     def _get_error_index(y_t, label):
         t = F.softmax(y_t, dim=1)
         t_argmax = torch.argmax(t, dim=1)
-        mask = torch.eq(label, t_argmax).float()
-        mask = mask.unsqueeze(-1)
-        error_index = [i for i, x in enumerate(mask) if x == 0]
+        error = torch.eq(label, t_argmax).float()
+        error = error.unsqueeze(-1)
+        error_index = [i for i, x in enumerate(error) if x == 0]
         return error_index
 
-    def forward(self, y_s, y_t, label):
-        error_index = self.get_error_index(y_t, label)
-        if len(error_index):
-            for s, t in zip(y_s, y_t):
-                for j in error_index:
-                    s[j].data *= 0
-                    t[j].data *= 0
-
-        y_s = y_s.mean(dim=1, keepdim=False)
-        y_t = y_t.mean(dim=1, keepdim=False)
+    def forward(self, y_t, f_s, model_t, opt):
+        # error_index = self.get_error_index(y_t, label)
+        # if len(error_index):
+        #     for s, t in zip(y_s, y_t):
+        #         for j in error_index:
+        #             s[j].data *= 0
+        #             t[j].data *= 0
+        tensor_l = []
+        for i, f in enumerate(f_s):
+            b, c, h, w = f.shape
+            f = f.mean(dim=1, keepdim=False).view(b, h * w)
+            if opt.model_t.__contains__('vgg'):
+                f = model_t.classifier(self.embed_s[i](f))
+            elif opt.model_t.__contains__('ResNet'):
+                f = model_t.linear(self.embed_s[i](f))
+            else:
+                f = model_t.fc(self.embed_s[i](f))
+            tensor_l.append(f @ y_t.t())  # 64 * 64
 
 
 if __name__ == '__main__':
-
     parser = argparse.ArgumentParser('argument for training')
-    parser.add_argument('--batch_size', type=int, default=64, help='batch_size')
-    parser.add_argument('--dataset', type=str, default='CIFAR100', choices=['CIFAR100', 'CIFAR10'], help='dataset')
+    parser.add_argument('--model_t', type=str, default='', help='')
     opt = parser.parse_args()
 
-    train_loader, val_loader, n_cls = getDataLoader(opt)
+    x = torch.randn(64, 3, 32, 32)
 
-    for idx, data in enumerate(train_loader):
-        input, target = data
-        if torch.cuda.is_available():
-            input = input.cuda()
-            target = target.cuda()
-        print(input.shape)
-        print(target.shape)
+    s_net = mobile_half(num_classes=100)
+    t_net = resnet32x4(num_classes=100)
+
+    f_s, s_logit = s_net(x, is_feat=True, preact=False)
+    f_t, t_logit = t_net(x, is_feat=True, preact=False)
+
+    gskd = GSKD(f_s[:-1], f_t[-1])
+
+    gskd(t_logit, f_s[:-1], t_net, opt)
